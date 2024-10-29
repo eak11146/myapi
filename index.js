292e925489667cbd8e7aa24f64d0feec0c5c8797
eak11146 <evo008hp@gmail.com>
2024-10-29 11:51:36 +0700

    add post path

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const mysql =require('mysql2')
-require('dotenv').config()
+const mysql =require('mysql2');
+require('dotenv').config();
 
+const bodyParser = require('body-parser');
 
 const app = express();
+/* // Middleware เพื่อให้สามารถอ่านข้อมูลแบบ JSON และ x-www-form-urlencoded ได้
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // เพิ่ม middleware นี้ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
+});
 
 app.get('/',function(req,res,next ){
     res.json ({ msg : "Hello !!"})
-}) 
+});
 
 
 app.get('/attractions',function(req,res,next){
@@ -20,7 +32,7 @@ app.get('/attractions',function(req,res,next){
             res.json(results)
         }
     )
-})
+});
 
 app.get('/attractions/:id',function(req,res,next){
     const id = req.params.id
@@ -30,6 +42,36 @@ app.get('/attractions/:id',function(req,res,next){
             res.json(results[0])
         }
     )
-})
+});
+
+
+
+// Create
+app.post('/attractions/post', (req, res) => {
+    const { name, detail,coverimage,latitude,longitude } = req.body;
+    
+    const sql = 'INSERT INTO attractions (name,detail,coverimage,latitude,longitude) VALUES (?, ?, ?, ?, ?)';
+    connection.query(sql, [name,detail,coverimage,latitude,longitude], (err, result) => {
+        if (err) throw err;
+        res.send('Place added');
+        if (!req.body.name) {
+            return res.status(400).send({ error: "Name is required" });
+        }else{
+            console.log(`Received name: ${name}, detail: ${detail} , coverimage: ${ coverimage } `);
+        }
+    }); 
+    /* const { name, detail } = req.body; // รับข้อมูลจาก request body
+    console.log(`Received name: ${name}, detail: ${detail}`);
+
+    // ส่ง response กลับไปยัง client
+    res.status(201).json({
+        message: 'Data received successfully!',
+        data: {
+            name,
+            detail,
+        }
+    }); */
+});
+
 
 app.listen(process.env.PORT || 3000);
